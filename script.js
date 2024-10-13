5dfeff4f97b6785ec90d784fa5d880bbc73887c3
Recheal Gademeh <rechealgademehetornam@gmail.com>
2024-10-13 20:43:19 +0000

    final commit

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 document.addEventListener("DOMContentLoaded", DOMContent);
 
+let tasks = []
+
 function DOMContent() {
   const addButton = document.getElementById("add-task-btn");
   const taskInput = document.getElementById("task-input");
   const taskList = document.getElementById("task-list");
+
+  const rawTask = localStorage.getItem("tasks") || '[]'
+   tasks = JSON.parse(rawTask);
+
+  tasks.forEach((element,index) => {
+    const li = document.createElement("li");
+    li.textContent = element;
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.classList.add("remove-btn");
+    removeButton.addEventListener("click", (event) => {
+      //remove logic
+      taskList.removeChild(li);
+      tasks = tasks.filter((v,i)=>i!=index);
+      const rawTask = JSON.stringify(tasks);
+      localStorage.setItem("tasks",rawTask)
+    });
+  
+    li.appendChild(removeButton);
+    taskList.appendChild(li);
+  });
 
   addButton.addEventListener("click", addTask);
   taskInput.addEventListener("keypress", (event) => {
@@ -28,9 +51,16 @@ function addTask() {
   const removeButton = document.createElement("button");
   removeButton.textContent = "Remove";
   removeButton.classList.add("remove-btn");
+  tasks.push(taskText);
+  const rawTask = JSON.stringify(tasks);
+  localStorage.setItem("tasks",rawTask)
+  const index = tasks.length - 1;
   removeButton.addEventListener("click", (event) => {
     //remove logic
     taskList.removeChild(li);
+    tasks = tasks.filter((v,i)=>i!=index);
+    const rawTask = JSON.stringify(tasks);
+    localStorage.setItem("tasks",rawTask)
   });
 
   li.appendChild(removeButton);
